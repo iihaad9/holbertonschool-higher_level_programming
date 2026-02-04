a2556d817258c97637edd32ecafc8a000595571a
iihaad9 <fahadfawaz5@gmail.com>
2026-02-04 15:13:21 +0000

    l

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -39,3 +39,21 @@ class Square:
             or value[0] < 0
             or value[1] < 0
         ):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """Print the square using # characters and position."""
+        if self.__size == 0:
+            print()
+            return
+
+        for _ in range(self.__position[1]):
+            print()
+
+        for _ in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
