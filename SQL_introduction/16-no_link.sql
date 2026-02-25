8fe12a60460a2f32e6bf522b898d0784ee897a76
iihaad9 <fahadfawaz5@gmail.com>
2026-02-25 20:33:13 +0000

    l

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
--- List all records with score >= 10 ordered by score desc
+-- List all records with a score >= 10 ordered by score DESC
 SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
