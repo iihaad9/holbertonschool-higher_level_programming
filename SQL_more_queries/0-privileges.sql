065b9fe385d2d7a4de131f11f77a88921a60fbac
iihaad9 <fahadfawaz5@gmail.com>
2026-02-26 20:28:51 +0000

    l

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- List privileges for user_0d_1 and user_0d_2 on localhost
+-- user_0d_1 privileges
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- user_0d_2 privileges
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
